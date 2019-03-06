fb0e70c6f7718a9380bbadc93d9d5ade6105a0d7
Saul Bernaber <sb883@cornell.edu>
2019-03-05 21:45:44 -0500

    Update Fluidization Code - no More AIDE-Design package for Sig Figs

--- Code/FluidizationVelocity.py
+++ Code/FluidizationVelocity.py
@@ -30,37 +30,19 @@ def FluidizationVelocity(area_reactor, porosity, density_sand, diameter, viscosi
 The reactor's fluidization velocity is 8464 millimeter / second.
 The reactor's fluidization flow is 42.32 milliliter / second.
   """
-
     area_reactor = float(input("\nWhat is the cross sectional area of the" + " fluidized bed reactor, in units of millimeters squared?\n"))*u.mm**2
-
-porosity = float(input("What is the porosity of the sand bed?\n"))
-
-    density_sand = float(input("What is the density of the sand, in units of" +
-    "kilograms per cubic meter?\n"))*u.kg/(u.m**3)
-
+    porosity = float(input("What is the porosity of the sand bed?\n"))
+    density_sand = float(input("What is the density of the sand, in units of" + "kilograms per cubic meter?\n"))*u.kg/(u.m**3)
     density_water = 997*u.kg/(u.m**3)
-
-    diameter = float(input("What is the average diameter of the sand grains," +
-    "in units of millimeters?\n")) * u.mm
-
+    diameter = float(input("What is the average diameter of the sand grains," + "in units of millimeters?\n")) * u.mm
     g = 9.8 * u.m/(u.s**2)
-
     kozeny = 5  # This is an approx. value, suggested by Fluidization Source 4
-
-    viscosity = float(input("What is the kinematic viscosity of water, in" +
-    "units of millimeters squared per second?\n"))*u.mm**2/u.s
-
-# The following variable definitions are based off those in the equation in
-# Fluidization Source 4.
-
+    viscosity = float(input("What is the kinematic viscosity of water, in" + "units of millimeters squared per second?\n"))*u.mm**2/u.s
+    # The following variable definitions are based off those in the equation in
+    # Fluidization Source 4.
     fluidization_velocity_FirstTerm = (porosity**3 * g * (diameter)**2)/(36*kozeny*(viscosity)*(1-porosity))
-
     fluidization_velocity_SecondTerm = (density_sand/density_water - 1)
-
     fluidization_velocity = fluidization_velocity_FirstTerm *fluidization_velocity_SecondTerm * (1000*u.mm)/(1*u.m)
-
     fluidization_flow = fluidization_velocity * area_reactor * (0.001*u.mL)/(1*u.mm**3)
-
     print("\nThe reactor's fluidization velocity is " + str(fluidization_velocity)+".")
-
     print("The reactor's fluidization flow is " + str(fluidization_flow) + ".")
