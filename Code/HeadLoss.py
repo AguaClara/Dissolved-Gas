e5bd3af857d0e2bd947240ed8173021fadc40d8a
Saul Bernaber <sb883@cornell.edu>
2019-03-28 16:37:19 -0400

    recording data

--- Code/HeadLoss.py
+++ Code/HeadLoss.py
@@ -1,10 +1,8 @@
 ##This code follows the formula provided by Ambient Pressure & Gas Solubility Source 5.
 
+from aguaclara.play import*
 import math
 import numpy as nm
-import aide_design as ad
-from aide_design import utility as ut
-from aide_design.play import*
 
 viscosity = float(input("What is the dynamic (i.e. absolute) viscosity of water, in units of millipascal seconds?\n"))*(1/1000) * u.kg/u.m/u.s
 effluent_tubing_length = float(input("What is the length of the effluent tubing, in units of meters?\n"))*u.m
@@ -16,4 +14,4 @@ pipe_diameter = float(input("What is the diameter of the effluent piping, in uni
 
 Head_Loss = (32 * viscosity * effluent_tubing_length * velocity)/(density_water * g * pipe_diameter**2)
 
-print("The Head Loss due to the effluent tubing is "+ str(ut.sig(Head_Loss,3))+".")
+print("The Head Loss due to the effluent tubing is "+ str((Head_Loss,3))+".")
