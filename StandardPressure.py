1d51a7598f78314b1b8723aee5d92075847b1752
Saul Bernaber <sb883@cornell.edu>
2019-04-17 00:25:08 -0400

    Ignore this until I figure it out. -Saul (sb883)

--- StandardPressure.py
+++ StandardPressure.py
@@ -9,8 +9,8 @@ print(data.columns) # Run this to see the column labels
 
 # Read the 0th column (time) and the 4th column (effluent turbidity)
 time, effluent_turbidity = pp.get_data_by_time(
-      path="TestingDO_HotWater_Trial02_20190412", columns=[0, 4], start_date="4-12-2019",
-      start_time="15:40", end_time="23:30")
+      path="data", columns=[0, 4], start_date="",
+      start_time="0.756646216", end_time="0.765037785")
 elapsed_time = (np.array(time)-time[0])*24
 
 plt.xlabel("Time (hours)")
